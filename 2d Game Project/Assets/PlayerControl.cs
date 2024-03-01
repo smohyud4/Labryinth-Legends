9620f5958e1f43328be468584398ebca9259a25e
BrianMerino <brianmerino08@gmail.com>
2024-03-01 11:27:37 -0500

    Player now has animation in all directions. Need to slow down the animations and player speed.

--- 2d Game Project/Assets/PlayerControl.cs
+++ 2d Game Project/Assets/PlayerControl.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float move_speed;
-    float speed_x, speed_y;
+    public float Move_speed = 5;
+    Vector2 movement;
     public Rigidbody2D rb;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +17,14 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        speed_x = Input.GetAxisRaw("Horizontal") * move_speed * Time.deltaTime;
-        speed_y = Input.GetAxisRaw("Vertical") * move_speed * Time.deltaTime;
-        rb.velocity = new Vector2(speed_x, speed_y);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate(){
+        rb.MovePosition(rb.position+movement * Move_speed * Time.fixedDeltaTime);
     }
 }
--- 2d Game Project/Assets/squaremove.cs
+++ 2d Game Project/Assets/squaremove.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class squaremove : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public GameObject square;
     void Start()
     {
         
