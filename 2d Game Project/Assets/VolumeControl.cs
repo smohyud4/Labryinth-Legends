05984121194f10c4e6d7a2a3c32e2915478829b9
smohyud1 <smohyud1@vols.utk.edu>
2024-03-05 22:01:33 -0500

    Main Menu Volume Settings

---
+++ 2d Game Project/Assets/VolumeControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Slider volumeSlider;
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            // Set Default to be max volume
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+
+        Load();
+    }
+
+    // Update is called once per frame
+    public void changeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
