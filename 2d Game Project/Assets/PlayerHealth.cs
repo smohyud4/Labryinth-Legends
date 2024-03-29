d397ba5a3aa7a4826f8fb67561f2dea76bba3ed8
smohyud1 <smohyud1@vols.utk.edu>
2024-03-29 18:28:42 -0500

    Adjust Player Health Script

--- 2d Game Project/Assets/PlayerHealth.cs
+++ 2d Game Project/Assets/PlayerHealth.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField ]private float health = 100f;
     [SerializeField] private float maxHealth = 100f;
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        // TODO: When Player gets attacked
+
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+			UpdateHealth(-10);
+		}
     }
     public void UpdateHealth(float mod){
+
         health += mod;
         if (health > maxHealth){
             health = maxHealth;
@@ -26,5 +36,7 @@ public class PlayerHealth : MonoBehaviour
             health = 0f;
             // Die();
         }
+
+        healthBar.SetHealth(health);
     }
 }
